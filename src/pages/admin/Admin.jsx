c516662be76cfe69fce301076d43d41045e73641
stefine727 <418554980@qq.com>
2020-06-22 23:53:49 +0800

    login

---
+++ src/pages/admin/Admin.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Admin extends Component {
+  state = {};
+  render() {
+    return <div>管理页面</div>;
+  }
+}
+
+export default Admin;
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './login.less'
+
+class Login extends Component {
+    state = {};
+    render() {
+        return (
+            <div className="login">
+                <header className="login-header"></header>
+                <header className="login-content"></header>
+            </div>
+        )
+    }
+}
+
+export default Login;
