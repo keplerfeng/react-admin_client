89b0d43e8db38b2809fb71e24010e8bb4d96865a
stefine727 <418554980@qq.com>
2020-06-23 23:53:51 +0800

    登录页面

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './login.less'
+import LoginForm from './loginForm/LoginForm'
 
 class Login extends Component {
     state = {};
     render() {
         return (
             <div className="login">
-                <header className="login-header"></header>
-                <header className="login-content"></header>
+                <header className="login-header"><h1>后台管理</h1></header>
+                <header className="login-content">
+                    <h2>用户登录</h2>
+                    <LoginForm className="login-form"></LoginForm>
+                </header>
             </div>
         )
     }
